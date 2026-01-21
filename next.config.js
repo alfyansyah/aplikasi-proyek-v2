e5af30603c9bcc625c214c48a8ef3357b381162c
alfyansyah <alfyansyah.syah@gmail.com>
2026-01-21 06:32:41 -0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public', // Menyimpan file service worker di folder public
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // PWA mati saat coding, nyala saat deploy
+});
+
 const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  // Izinkan gambar dari Supabase
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
