06e66c19aecab71cac67b4fff93b1c5715681f20
alfyansyah <alfyansyah.syah@gmail.com>
2026-01-25 05:08:10 -0800

    logo beck

--- app/components/BrandBackground.tsx
+++ app/components/BrandBackground.tsx
@@ -2,7 +2,7 @@
 
 export default function BrandBackground() {
   // 🔴 GANTI LINK INI DENGAN URL LOGO BRAND ANDA SENDIRI
-  const MY_BRAND_LOGO = "https://prvzdhyyblbsahaxkjlc.supabase.co/storage/v1/object/public/app-assets/logo%20contech.png"; 
+  const MY_BRAND_LOGO = "https://prvzdhyyblbsahaxkjlc.supabase.co/storage/v1/object/public/app-assets/logo-contech-beckground.png"; 
 
   return (
     <div className="fixed inset-0 z-[-1] flex items-center justify-center pointer-events-none overflow-hidden bg-slate-50">
